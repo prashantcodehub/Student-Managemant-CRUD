aca05a36c924df1f5056fa7bafb8c45ca77244f6
prashantcodehub <113547581+prashantcodehub@users.noreply.github.com>
2023-07-04 06:18:32 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const students = [ 
+    { ID: 1, name: 'Alice', email: 'alice@example.com', age: 21, grade: 'A', degree: 'Btech'}, 
+    { ID: 2, name: 'Bob', email: 'bob@example.com' , age: 22, grade: 'B', degree: 'MBA'}, 
+    { ID: 3, name: 'Charlie', email: 'charlie@example.com', age: 20, grade: 'C', degree:'Arts'} 
+];
+
+const NAME = document.getElementById('name');
+const EMAIL = document.getElementById('email');
+const CGPA = document.getElementById('GPA');
+const AGE = document.getElementById('age');
+const DEGREE = document.getElementById('degree');
+
